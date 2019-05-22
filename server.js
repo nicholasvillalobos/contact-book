7a21ad8f702abe1c7422b1892e9990f9aa5e2bcc
Nicholas Villalobos <nicholasvillalobos298@gmail.com>
2019-05-22 18:03:52 -0400

    added user model

--- server.js
+++ server.js
@@ -1,10 +1,46 @@
 const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const testing = require(path.join(__dirname, '..', 'contact-book', 'models', 'testing.js'))
+const User = require('.\\models\\User')
+
 const portnum = process.env.PORT || 3000
+
+testing()
 
 const app = express()
 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://nicholasvillalobos:%23Xboxcrazy30@cluster0-wdkku.mongodb.net/test?retryWrites=true', { useNewUrlParser: true})
+const db = mongoose.connection
+db.on('error', error => console.error('db failed to open'))
+db.once('open', () => console.log('db opened successfully'))
+
 app.get('/', (req, res, next) => {
   res.send("welcome to the contact-book")
+})
+
+app.post('/users/', (req, res, next) => {
+  // take info given, format, and send to data base
+  User.create({
+    name: req.body.name,
+    phone: req.body.phone,
+    email: req.body.email,
+    password: req.body.password
+  }, (err, user) => {
+    if (err) return res.status(500).send("error making user");
+    res.status(200).send(user);
+  })
+})
+
+app.get('/users/', (req, res, next) => {
+  User.find({}, (err, users) => {
+    if (err) return res.status(500).send("couldn't get user")
+    res.status(200).send(users)
+  })
 })
 
 const server = app.listen(portnum, () => {
